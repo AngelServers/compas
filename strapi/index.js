7000b2add26ab4a55a18e385b596be1416ccade2
Angel Zimmermann <angel.pereira.riv@gmail.com>
2024-01-12 20:03:28 +0100

    Build

--- strapi/index.js
+++ strapi/index.js
@@ -1,104 +1,139 @@
-(() => {
-  // src/strapi/sanitizer.ts
-  var parseServiceResult = (result) => {
-    return { data: result.results, meta: { pagination: result.pagination } };
-  };
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var __publicField = (obj, key, value) => {
+  __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
+  return value;
+};
 
-  // src/strapi/role-manager.ts
-  var RoleManager = class {
-    strapi;
-    roleId;
-    role;
-    roleName;
-    constructor(strapi) {
-      this.strapi = strapi;
+// strapi/main.ts
+var main_exports = {};
+__export(main_exports, {
+  RoleManager: () => RoleManager,
+  parseServiceResult: () => parseServiceResult
+});
+module.exports = __toCommonJS(main_exports);
+
+// src/strapi/sanitizer.ts
+var parseServiceResult = (result) => {
+  return { data: result.results, meta: { pagination: result.pagination } };
+};
+
+// src/strapi/role-manager.ts
+var RoleManager = class {
+  constructor(strapi) {
+    __publicField(this, "strapi");
+    __publicField(this, "roleId");
+    __publicField(this, "role");
+    __publicField(this, "roleName");
+    this.strapi = strapi;
+  }
+  async FindRole(name) {
+    var _a;
+    const allRoles = await this.strapi.service("plugin::users-permissions.role").find();
+    this.roleId = (_a = allRoles.filter(
+      (role) => role.name === name
+    )[0]) == null ? void 0 : _a.id;
+    if (!this.roleId) {
+      this.strapi.log.error(`No se encontro el Role ID del Role ${name}`);
+      return false;
+    } else {
+      return true;
     }
-    async FindRole(name) {
-      const allRoles = await this.strapi.service("plugin::users-permissions.role").find();
-      this.roleId = allRoles.filter(
-        (role) => role.name === name
-      )[0]?.id;
-      if (!this.roleId) {
-        this.strapi.log.error(`No se encontro el Role ID del Role ${name}`);
-        return false;
-      } else {
-        return true;
-      }
+  }
+  async SetRole(name) {
+    if (!await this.FindRole(name))
+      return;
+    const selectedRoleData = await this.strapi.service("plugin::users-permissions.role").findOne(this.roleId);
+    this.role = selectedRoleData;
+    this.roleName = selectedRoleData.name;
+    return selectedRoleData !== null;
+  }
+  async FindOrCreateRole(name, description) {
+    if (!await this.FindRole(name)) {
+      await this.strapi.service("plugin::users-permissions.role").createRole({
+        name,
+        description
+      });
+    } else {
+      this.SetRole(name);
     }
-    async SetRole(name) {
-      if (!await this.FindRole(name))
-        return;
-      const selectedRoleData = await this.strapi.service("plugin::users-permissions.role").findOne(this.roleId);
-      this.role = selectedRoleData;
-      this.roleName = selectedRoleData.name;
-      return selectedRoleData !== null;
-    }
-    async FindOrCreateRole(name, description) {
-      if (!await this.FindRole(name)) {
-        await this.strapi.service("plugin::users-permissions.role").createRole({
-          name,
-          description
-        });
-      } else {
-        this.SetRole(name);
-      }
-    }
-    async SetPermissionState(controller, permission, state, type = "api", service = null) {
-      const setPerm = (perm) => {
-        const serviceType = `${type}::${service ? service : controller}`;
-        if (!this.role)
-          this.strapi.log.error("\u{1F512} No hay role configurado");
-        else if (!this.role.permissions)
-          this.strapi.log.error("\u{1F512} El role no tiene permisos");
-        else if (!this.role.permissions[serviceType])
-          this.strapi.log.error(
-            `\u{1F512} El tipo de servicio ${serviceType} no se encontr\xF3`
-          );
-        else if (!this.role.permissions[serviceType].controllers)
-          this.strapi.log.error(
-            `\u{1F512} El tipo de servicio ${serviceType} no tiene controllers definidos`
-          );
-        else if (!this.role.permissions[serviceType].controllers[controller])
-          this.strapi.log.error(
-            `\u{1F512} El controller ${controller} en el servicio ${serviceType} no se encontr\xF3`
-          );
-        else if (!this.role.permissions[serviceType].controllers[controller][perm])
-          this.strapi.log.error(
-            `\u{1F512} El permiso ${perm} del controller ${controller} no se encontr\xF3`
-          );
-        else {
-          this.role.permissions[serviceType].controllers[controller][perm].enabled = state;
-        }
-      };
-      if (Array.isArray(permission)) {
-        permission.map((permission2) => {
-          setPerm(permission2);
-        });
-      } else {
-        setPerm(permission);
-      }
-    }
-    // Allow permission for everything
-    async SetSuperUser() {
-      if (!this.role) {
+  }
+  async SetPermissionState(controller, permission, state, type = "api", service = null) {
+    const setPerm = (perm) => {
+      const serviceType = `${type}::${service ? service : controller}`;
+      if (!this.role)
         this.strapi.log.error("\u{1F512} No hay role configurado");
-        return;
+      else if (!this.role.permissions)
+        this.strapi.log.error("\u{1F512} El role no tiene permisos");
+      else if (!this.role.permissions[serviceType])
+        this.strapi.log.error(
+          `\u{1F512} El tipo de servicio ${serviceType} no se encontr\xF3`
+        );
+      else if (!this.role.permissions[serviceType].controllers)
+        this.strapi.log.error(
+          `\u{1F512} El tipo de servicio ${serviceType} no tiene controllers definidos`
+        );
+      else if (!this.role.permissions[serviceType].controllers[controller])
+        this.strapi.log.error(
+          `\u{1F512} El controller ${controller} en el servicio ${serviceType} no se encontr\xF3`
+        );
+      else if (!this.role.permissions[serviceType].controllers[controller][perm])
+        this.strapi.log.error(
+          `\u{1F512} El permiso ${perm} del controller ${controller} no se encontr\xF3`
+        );
+      else {
+        this.role.permissions[serviceType].controllers[controller][perm].enabled = state;
       }
-      Object.values(this.role.permissions).map((permission) => {
-        Object.values(permission.controllers).map((controller) => {
-          Object.values(controller).map((perm) => {
-            perm.enabled = true;
-          });
+    };
+    if (Array.isArray(permission)) {
+      permission.map((permission2) => {
+        setPerm(permission2);
+      });
+    } else {
+      setPerm(permission);
+    }
+  }
+  // Allow permission for everything
+  async SetSuperUser() {
+    if (!this.role) {
+      this.strapi.log.error("\u{1F512} No hay role configurado");
+      return;
+    }
+    Object.values(this.role.permissions).map((permission) => {
+      Object.values(permission.controllers).map((controller) => {
+        Object.values(controller).map((perm) => {
+          perm.enabled = true;
         });
       });
-    }
-    async Save() {
-      await this.strapi.service("plugin::users-permissions.role").updateRole(this.roleId, this.role).then((res) => {
-        this.strapi.log.info(
-          `\u{1F512} Permisos para role ${this.roleName} actualizados!`
-        );
-      }).catch((e) => {
-      });
-    }
-  };
-})();
+    });
+  }
+  async Save() {
+    await this.strapi.service("plugin::users-permissions.role").updateRole(this.roleId, this.role).then((res) => {
+      this.strapi.log.info(
+        `\u{1F512} Permisos para role ${this.roleName} actualizados!`
+      );
+    }).catch((e) => {
+    });
+  }
+};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  RoleManager,
+  parseServiceResult
+});
