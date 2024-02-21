38c4ad649f80e4fde86802f8b1a7f5ce7804fee5
Angel Zimmermann <angel.pereira.riv@gmail.com>
2024-02-21 23:01:48 +0100

    Testing .d.ts

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export { compas } from "./src/CompasProvider";
+
+// Helpers
+export { Rut } from "./src/general/rut";
+
+// Components for f7 (Framework7)
+export { Table } from "./src/general/components/f7/Table";
