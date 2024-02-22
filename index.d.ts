26ca38ecb5713c5f605e8e19f7721a3e47a97d84
Angel Zimmermann <angel.pereira.riv@gmail.com>
2024-02-22 02:31:55 +0100

    Delete index.d.ts

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-export { compas } from "./src/CompasProvider";
-
-// Helpers
-export { Rut } from "./src/general/rut";
-
-// Components for f7 (Framework7)
-export { Table } from "./src/general/components/f7/Table";
