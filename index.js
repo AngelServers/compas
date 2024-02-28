38f9dde721fae8bbd75ee7ee27677cd59d34110b
Angel Zimmermann <angel.pereira.riv@gmail.com>
2024-02-28 14:58:53 +0100

    Exporting new components

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Constants
-export { compas } from "./src/CompasProvider";
+export { CompasProvider } from "./src/CompasProvider";
 
 // Helpers
 export { Rut } from "./src/general/rut";
@@ -7,6 +7,8 @@ export { Rut } from "./src/general/rut";
 // Components for f7 (Framework7)
 export { Table } from "./src/general/components/f7/Table";
 export { TablePage } from "./src/general/components/f7/TablePage";
+export { FormEditor } from "./src/general/components/f7/FormEditorOld";
+export { RecordEditor } from "./src/general/components/f7/RecordEditor";
 
 // WIP: TablePageComponent
 // import TablePageComponent from "./src/general/components/f7/TablePage";
