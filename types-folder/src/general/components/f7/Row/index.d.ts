b806a778474c84d726a2085b3bf5cd48bca3611a
Angel Zimmermann <angel.pereira.riv@gmail.com>
2024-03-12 10:29:43 +0100

    Adding type for last commit

--- types-folder/src/general/components/f7/Row/index.d.ts
+++ types-folder/src/general/components/f7/Row/index.d.ts
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.scss";
 export declare const Row: ({ cols, small, medium, large, children, }: {
     cols: number;
-    children: React.ReactNode[];
+    children: React.ReactNode | React.ReactNode[];
     small?: number;
     medium?: number;
     large?: number;
